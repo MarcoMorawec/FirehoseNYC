e6f9006140020a447cc983ea7904cf26d86ac6bf
marcomorawec <marco.morawec@gmail.com>
2013-10-19 12:05:49 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firehosenyc::Application.config.secret_token = '64ed1453c18dae8618d6ad91ca1fbd13e599a0f35168dd66fe28904a2b3b55933a611a73e411210704465b1886547be8e326d4a5d83b13102dc53ba3dabee806'
